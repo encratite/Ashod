37e8c4e86a3d759437e91abf0b2b53d195c898d9
void <git.email.address@googlemail.com>
2014-04-21 22:45:11 +0200

    Added a serialisable dictionary

---
+++ Ashod/SerializableDictionary.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace Ashod
+{
+	[XmlRoot("dictionary")]
+	public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IXmlSerializable
+	{
+		private const string ItemTag = "Item";
+		private const string KeyTag = "Key";
+		private const string ValueTag = "Value";
+
+		public System.Xml.Schema.XmlSchema GetSchema()
+		{
+			return null;
+		}
+
+		public void ReadXml(System.Xml.XmlReader reader)
+		{
+			XmlSerializer keySerialiser = new XmlSerializer(typeof(TKey));
+			XmlSerializer valueSerialiser = new XmlSerializer(typeof(TValue));
+
+			bool wasEmpty = reader.IsEmptyElement;
+			reader.Read();
+
+			if (wasEmpty)
+				return;
+
+			while (reader.NodeType != System.Xml.XmlNodeType.EndElement)
+			{
+				reader.ReadStartElement(ItemTag);
+
+				reader.ReadStartElement(KeyTag);
+				TKey key = (TKey)keySerialiser.Deserialize(reader);
+				reader.ReadEndElement();
+
+				reader.ReadStartElement(ValueTag);
+				TValue value = (TValue)valueSerialiser.Deserialize(reader);
+				reader.ReadEndElement();
+
+				this.Add(key, value);
+
+				reader.ReadEndElement();
+				reader.MoveToContent();
+			}
+			reader.ReadEndElement();
+		}
+
+		public void WriteXml(System.Xml.XmlWriter writer)
+		{
+			XmlSerializer keySerialiser = new XmlSerializer(typeof(TKey));
+			XmlSerializer valueSerialiser = new XmlSerializer(typeof(TValue));
+
+			foreach (TKey key in this.Keys)
+			{
+				writer.WriteStartElement(ItemTag);
+
+				writer.WriteStartElement(KeyTag);
+				keySerialiser.Serialize(writer, key);
+				writer.WriteEndElement();
+
+				writer.WriteStartElement(ValueTag);
+				TValue value = this[key];
+				valueSerialiser.Serialize(writer, value);
+				writer.WriteEndElement();
+
+				writer.WriteEndElement();
+			}
+		}
+	}
+
+
+}
---
+++ Ashod/XmlFile.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace Ashod
+{
+	public static class XmlFile
+	{
+		public const string DefaultPath = "Configuration.xml";
+
+		public static void Write<SerializedType>(SerializedType input, string path = DefaultPath)
+		{
+			using (var stream = new StreamWriter(path))
+			{
+				var serialiser = new XmlSerializer(typeof(SerializedType));
+				serialiser.Serialize(stream, input);
+			}
+		}
+
+		public static SerializedType Read<SerializedType>(string path = DefaultPath)
+		{
+			using (var stream = new StreamReader(path))
+			{
+				var serialiser = new XmlSerializer(typeof(SerializedType));
+				var output = (SerializedType)serialiser.Deserialize(stream);
+				return output;
+			}
+		}
+	}
+}
