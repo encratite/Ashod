2d200c76be457d1782a11f7dd0290caba05abb19
void <git.email.address@googlemail.com>
2014-07-04 16:13:02 +0200

    Database updates

--- Ashod/Database/DatabaseFactory.cs
+++ Ashod/Database/DatabaseFactory.cs
@@ -1,15 +1,38 @@
-﻿using System.Data.Common;
-using System.Data.SqlClient;
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
 
 namespace Ashod.Database
 {
-	public class DatabaseFactory
+	public class DatabaseFactory : IDisposable
 	{
 		private DbConnection _Connection;
 
 		public DatabaseFactory(DbConnection connection)
 		{
 			_Connection = connection;
+			if (_Connection.State != ConnectionState.Open)
+			{
+				try
+				{
+					_Connection.Open();
+				}
+				catch
+				{
+					_Connection.Dispose();
+					throw;
+				}
+			}
+		}
+
+		public void Dispose()
+		{
+			if (_Connection != null)
+			{
+				_Connection.Dispose();
+				_Connection = null;
+			}
 		}
 
 		public DbCommand Command(string commandText, params CommandParameter[] parameters)
@@ -26,6 +49,15 @@ namespace Ashod.Database
 			return command;
 		}
 
+		public DbCommand Function(string function, params CommandParameter[] parameters)
+		{
+			var newParameters = parameters.Select(x => new CommandParameter("@" + x.Name, x.Value)).ToArray();
+			string parameterString = string.Join(", ", newParameters.Select(x => x.Name).ToArray());
+			string commandText = string.Format("select {0}({1})", function, parameterString);
+			var command = Command(commandText, newParameters);
+			return command;
+		}
+
 		public DatabaseReader Reader(string commandText, params CommandParameter[] parameters)
 		{
 			var command = Command(commandText, parameters);
@@ -39,6 +71,19 @@ namespace Ashod.Database
 			{
 				command.ExecuteNonQuery();
 			}
+		}
+
+		public void NonQueryFunction(string function, params CommandParameter[] parameters)
+		{
+			using (var command = Function(function, parameters))
+			{
+				command.ExecuteNonQuery();
+			}
+		}
+
+		public DbTransaction Transaction()
+		{
+			return _Connection.BeginTransaction();
 		}
 	}
 }
