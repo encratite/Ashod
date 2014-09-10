c36cf4892d53f63446df1f2cbbce48f4a72c888b
void <git.email.address@googlemail.com>
2014-09-10 12:49:58 +0200

    Added a JSON reader/writer

---
+++ Ashod/JsonFile.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Ashod
+{
+    public static class JsonFile
+    {
+        public const string DefaultPath = "Configuration.json";
+
+        public static void Write<SerializedType>(SerializedType input, string path = DefaultPath)
+        {
+            string data = JsonConvert.SerializeObject(input);
+            File.WriteAllText(path, data);
+        }
+
+        public static SerializedType Read<SerializedType>(string path = DefaultPath)
+        {
+            string data = File.ReadAllText(path);
+            var output = JsonConvert.DeserializeObject<SerializedType>(data);
+            return output;
+        }
+    }
+}
