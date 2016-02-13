8cc1b723d47be844f1d56c439b69f1ce012be429
void <git.email.address@googlemail.com>
2016-02-13 23:11:27 +0100

    Added InitializerDictionary

---
+++ Ashod/InitializerDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ashod
+{
+	public class InitializerDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+	{
+		public TValue Get(TKey key, Func<TValue> initializer)
+		{
+			if (initializer == null)
+				throw new ArgumentException("No initializer has been set.");
+			TValue value;
+			if (!TryGetValue(key, out value))
+			{
+				value = initializer();
+				this[key] = value;
+			}
+			return value;
+		}
+	}
+}
